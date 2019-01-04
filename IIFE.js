36c6c7c3744ca0db77bc900044a67156bd07014f
imranquadri3107 <imranquadri77@gmail.com>
2019-01-03 20:31:21 -0500

    Update IIFE.js

--- IIFE.js
+++ IIFE.js
@@ -20,3 +20,24 @@
   if (true) console.log('hello world');
   
 })();
+
+
+// Hoisting and IIFE
+
+// Scope a d Self
+
+var myCar = {
+  color: 'Blue',
+  logColor: function(){
+    var self = this;
+    console.log('In logColor - this.color:' + this.color);
+    console.log('In logColor - self.color:' + self.color);
+
+    (function(){
+      console.log('In IIFE - this.color:' + this.color);
+      console.log('In IIFE - self.color:' + self.color);
+    })();
+  }
+}
+
+myCar.logColor();
