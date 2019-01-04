54006c19d48a5562244d3c0d5c3b86cb1afd56cf
imranquadri3107 <imranquadri77@gmail.com>
2019-01-03 20:36:38 -0500

    Create ComparisonOperator.js

---
+++ ComparisonOperator.js
@@ -0,0 +1,6 @@
+// //Difference between == and === 
+// ==(equals) Test for abstract equality(doesn't test data type)
+// === test for strict equality(test data type)
+
+console.log(7 == '7');
+console.log(7 === '7');
