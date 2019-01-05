13dc91c9258cc6879fd632d64568bb52b7435e1e
imranquadri3107 <imranquadri77@gmail.com>
2019-01-05 17:38:57 -0500

    Create ArrayConstructor.js

---
+++ ArrayConstructor.js
@@ -0,0 +1,17 @@
+// Array Constructor
+
+//SIngle argument(number) -> returns empty array of that length
+// single argument(number) -> returns array with just that argument passed a an element
+
+//Multiple arguments passed -> returns new array with all arguments passed in as elements in it.
+
+var arr1 = [];
+var arr2 = new Array('Hello Imran');
+var arr3 = new Array(1, 2, 'three', 4, 'five');
+var arr4 = new Array([1, 2, 3, 4, 5]);
+
+console.log('arr1: ', arr1);
+console.log('arr2: ', arr2);
+console.log('arr3: ', arr3);
+console.log('arr4: ', arr4);
+
