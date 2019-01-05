b575bd090b2a963cd0efd09dd5d4d414e99bf4a6
imranquadri3107 <imranquadri77@gmail.com>
2019-01-05 18:11:42 -0500

    Create FloatingNumber.js

---
+++ FloatingNumber.js
@@ -0,0 +1,6 @@
+// In JavaScript all numbers are IEEE floating point numbers
+console.log(900.9 === 300.3 * 3);
+console.log(300.3 * 3);
+console.log(Number((300.3 * 3).toFixed(2)));
+console.log(Number((300.3 * 3).toPrecision(12)));
+console.log(((300.3 * 10) * 3) / 10);
