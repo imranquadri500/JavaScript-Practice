00cc1c72266cec55a7bae78206017fc179bb7302
imranquadri3107 <imranquadri77@gmail.com>
2019-01-04 23:34:02 -0500

    Update CallMethod.js

--- CallMethod.js
+++ CallMethod.js
@@ -15,3 +15,15 @@ const car2 = {
 }
 
 car1.getCarDescription.call(car2, 200000, 2018, 'Red');
+
+
+// Push()
+
+
+const list1 = [1, 2, 3, 4, 5];
+//const list2 = list1.concat([]);
+const list2 = list1.slice();
+
+list1.push(6, 7, 8);
+console.log(list1);
+console.log(list2);
