dc3094074339c9c30573e8c00a3f9f9f2b35457a
imranquadri3107 <imranquadri77@gmail.com>
2019-01-06 18:42:00 -0500

    Update setTimeout.js

--- setTimeout.js
+++ setTimeout.js
@@ -14,3 +14,21 @@ function logNumber(){
 }
 
 logNumber();
+
+
+////////////////////////////////////////
+//Another Example
+
+const s = new Date().getSeconds();
+
+setTimeout(function() {
+  // prints out "2", meaning that the callback is not called immediately after 500 milliseconds.
+  console.log("Ran after " + (new Date().getSeconds() - s) + " seconds");
+}, 500);
+
+while(true) {
+  if(new Date().getSeconds() - s >= 2) {
+    console.log("Good, looped for 2 seconds");
+    break;
+  }
+}
