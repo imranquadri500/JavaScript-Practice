7d813b559fd9470c40885e0d3baeb162cb1cac35
imranquadri3107 <imranquadri77@gmail.com>
2019-01-06 18:37:25 -0500

    Update ObjectProperties.js

--- ObjectProperties.js
+++ ObjectProperties.js
@@ -11,3 +11,19 @@ a[c] = 400; // a['object object'] = 400
 
 console.log(a[b]); // logs the recent set value
 // console.log(a['object object]');
+
+//////////////////////////////////////
+function Graph() {
+  this.vertices = [];
+  this.edges = [];
+}
+
+Graph.prototype = {
+  addVertex: function(v) {
+    this.vertices.push(v);
+  }
+};
+
+var g = new Graph();
+// g is an object with own properties 'vertices' and 'edges'.
+// g.[[Prototype]] is the value of Graph.prototype when new Graph() is executed.
