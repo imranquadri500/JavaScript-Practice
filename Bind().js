83149df30b7d50828b5138d57e2a2be458c4063a
imranquadri3107 <imranquadri77@gmail.com>
2019-01-05 21:13:20 -0500

    Update Bind().js

--- Bind().js
+++ Bind().js
@@ -16,3 +16,30 @@ const roadTrip2 = {
 
 const getTripDistance = roadTrip1.getDistance.bind(roadTrip2, ' km',);
 getTripDistance(' in total');
+
+
+// Example of two accounts 
+
+const account1 = {
+  name: 'Jen',
+  totalAmount: 5000,
+  deductAmount: function(amount){
+    this.totalAmount-= amount;
+    return 'Amount in account' + this.totalAmount;
+  },
+};
+
+const account2 = {
+  name: 'James',
+  totalAmount: 8000,
+};
+
+const withdrawAccount = function(amount){
+  return account1.deductAmount.bind(account2, amount);
+};
+
+console.log(withdrawAccount(500)());
+console.log(withdrawAccount(200)());
+
+//Output:  Amount in account: 7500
+//         Amount in account: 7300
